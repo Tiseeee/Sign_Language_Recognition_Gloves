83cf73fd66f8411e1d20b65f7f359682a075306a
Tise <17320417938@163.com>
2026-07-12 21:04:12 +0800

    更改位置

---
+++ Cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "h/HandStruct.h"
+#include "h/VectorNorm.h"
+#include "h/Data_input.h"
+using namespace std;
+
+int Recrod_Number=0;
+
+int main() {
+    Data_Input();
+    return 0;
+}
